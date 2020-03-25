3e4fb56aa70ebdc4483080218c7c8b83ac07cd9a
Brandon Marrero <brandonmar97@gmail.com>
2020-03-25 19:09:35 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,7 @@ class EmailAddressParser
   
   def initialize(emails)
     @emails = emails.split(/[,\s]+/)
-    @@parse << @emails.flatten
+    @@parse << @emails.flatten.uniq
   end
   
   def parse 
